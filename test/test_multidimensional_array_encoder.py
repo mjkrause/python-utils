93517d7454d5238ee0d54d3c852d6e4b44acc44d
Michael Krause <michael.j.krause@gmail.com>
2023-10-21 13:21:02 -0700

    All tests fixed

--- test/test_multidimensional_array_encoder.py
+++ test/test_multidimensional_array_encoder.py
@@ -31,28 +31,52 @@ class TestMultiDimensionalArrayEncoder(unittest.TestCase):
         #                  object_hook=hinted_tuple_hook, )
         #       )
 
-    def test_dict(self):
+    def test_dict_tuple_as_key_encoded(self):
+        expected = '{"{\\"__tuple__s_i_g_n_i_f_i_e_r__\\": \\"[2]\\"}": "a"}'
+        observed = f'{self.enc.encode(self.test_input_2)}'
+        self.assertEqual(expected, observed)
 
-        print('test_input_2 encoded:', self.enc.encode(self.test_input_2))
-        print('test_input_2 decoded:',
-              json.loads(self.enc.encode(self.test_input_2),
-                         object_hook=hinted_tuple_hook, )
-              )
+        # print('test_input_2 encoded:', self.enc.encode(self.test_input_2))
 
-    def test_dict_of_tuples(self):
+    def test_dict_tuple_as_key_decoded(self):
+        expected = {(2,): 'a'}
+        observed = json.loads(self.enc.encode(self.test_input_2), object_hook=hinted_tuple_hook, )
+        self.assertEqual(expected, observed)
 
-        print('test_input_3 encoded:', self.enc.encode(self.test_input_3))
-        print('test_input_3 decoded:',
-              json.loads(self.enc.encode(self.test_input_3),
-                         object_hook=hinted_tuple_hook, )
-              )
+        # print('test_input_2 decoded:',
+        #       json.loads(self.enc.encode(self.test_input_2),
+        #                  object_hook=hinted_tuple_hook, )
+        #       )
 
-    def test_dict_only(self):
-        print('test_input_4  encoded:', self.enc.encode(self.test_input_4))
-        print('test_input_4 decoded:',
-              json.loads(self.enc.encode(self.test_input_4),
-                         object_hook=hinted_tuple_hook, )
-              )
+    def test_dict_of_tuples_encode(self):
+        expected = '{"a": {"{\\"__tuple__s_i_g_n_i_f_i_e_r__\\": \\"[2]\\"}": {"1": "a"}}}'
+        observed = f'{self.enc.encode(self.test_input_3)}'
+        self.assertEqual(expected, observed)
+        # print('test_input_3 encoded:', self.enc.encode(self.test_input_3))
+
+    def test_dict_of_tuples_decode(self):
+        expected = {'a': {(2,): {'1': 'a'}}}
+        observed = json.loads(self.enc.encode(self.test_input_3), object_hook=hinted_tuple_hook, )
+        self.assertEqual(expected, observed)
+        # print('test_input_3 decoded:',
+        #       json.loads(self.enc.encode(self.test_input_3),
+        #                  object_hook=hinted_tuple_hook, )
+        #       )
+
+    def test_dict_encode(self):
+        expected = '{"a": "b"}'
+        observed = f'{self.enc.encode(self.test_input_4)}'
+        self.assertEqual(expected, observed)
+        # print('test_input_4  encoded:', self.enc.encode(self.test_input_4))
+
+    def test_dict_decode(self):
+        expected = {'a': 'b'}
+        observed = json.loads(self.enc.encode(self.test_input_4), object_hook=hinted_tuple_hook, )
+        self.assertEqual(expected, observed)
+        # print('test_input_4 decoded:',
+        #       json.loads(self.enc.encode(self.test_input_4),
+        #                  object_hook=hinted_tuple_hook, )
+        #       )
 
 
 if __name__ == '__main__':
